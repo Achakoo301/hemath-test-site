123f5946fff1bf8eb35b86bffc0cb7ef79bf6033
Achakoo301 <amitchakoo301@gmail.com>
2026-03-25 15:42:15 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+let cart = [];
+
+function addToCart(productName, price) {
+  cart.push({ productName, price });
+  alert(productName + " added to cart!");
+  console.log(cart);
+}alert("connected");
